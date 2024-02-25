77a9ad363a86b7e92bc76139075fcf80efc5a629
karseny99 <arseny.karimov@gmail.com>
2024-02-25 21:37:27 +0300

    optimization update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <Windows.h>
 #include <iostream>
-#include <chrono>
 #include <mmsystem.h>
 
 
@@ -9,22 +8,14 @@ using namespace std;
 // g++ -std=c++20 main.cpp -o timer -lWinmm -mwindows
 
 
-void fallAsleep(int time) {
-    auto start = std::chrono::high_resolution_clock::now();
-    std::chrono::minutes duration(time);
-
-    while(start + duration >= std::chrono::high_resolution_clock::now()) {}
-
-}
-
 int main() {
 
     system("cmd /k start");
 
     while(1) {
-        fallAsleep(30); // minutes
+        Sleep(1000 * 60 * 30); // 30 minutes
         PlaySound(TEXT("4.wav"), NULL, SND_FILENAME | SND_ASYNC);
-        fallAsleep(2);
+        Sleep(1000 * 60 * 2); // 2 minutes
         PlaySound(TEXT("2.wav"), NULL, SND_FILENAME | SND_ASYNC);
     }
 
